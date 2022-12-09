1dbad7b225faa87b2ce388b8bbb268be924209b5
Jason Godesky <jefgodesky@gmail.com>
2022-12-08 21:44:51 -0500

    Add current campaign to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,14 @@ export default function Home () {
     <>
       <PageHead />
       <Header />
+      <main className='home'>
+        <p><em>This website records the lore of one iteration of the world of <a href='/eberron'>Eberron</a>. There are many Eberrons, but this one is ours.</em></p>
+        <section className='current-campaign'>
+          <h2>Current Campaign</h2>
+          <img src='/img/campaigns/sword-and-shield.png' alt='Eberron: Sword &amp; Shield' />
+          <p><em>Two years after the signing of the <a href='/treaty-of-thronehold'>Treaty of Thronehold</a>, a few <a href='/breland/culture'>Brelish</a> patriots in <a href='/sharn'>Sharn</a> seek to join the <a href='/swords-of-liberty'>Swords of Liberty</a> to end the <a href='/monarchy'>monarchy</a> once and for all. It is the beginning of a tale of radicals and revolution that will change the City of Towers &mdash; and <a href='/eberron'>Eberron</a> &mdash; forever.</em></p>
+        </section>
+      </main>
     </>
   )
 }
