596edcecc6695bfff2ff59a54a353531607679a5
Jason Godesky <jefgodesky@gmail.com>
2022-12-08 21:18:15 -0500

    Add new component: PageHeadTwitterCard

---
+++ components/PageHeadTwitterCard/PageHeadTwitterCard.tsx
@@ -0,0 +1,22 @@
+interface PageHeadTwitterCardProps {
+  title?: string
+  description?: string
+  image?: string
+}
+
+export default function PageHeadTwitterCard (props: PageHeadTwitterCardProps) {
+  const title = props.title ?? 'The Dragon Between'
+  const description = props.description ?? 'Welcome to Eberron.'
+  const image = props.image ?? 'https://dragonbetween.net/img/social.jpg'
+
+  return (
+    <>
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:image' content={image} />
+    </>
+  )
+}
+
+export type { PageHeadTwitterCardProps }
