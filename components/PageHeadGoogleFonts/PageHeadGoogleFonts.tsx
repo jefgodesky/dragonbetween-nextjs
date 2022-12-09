a7be759ef66c4ec32675219ae271dad8a6ee64c0
Jason Godesky <jefgodesky@gmail.com>
2022-12-08 21:18:06 -0500

    Add new component: PageHeadGoogleFonts

---
+++ components/PageHeadGoogleFonts/PageHeadGoogleFonts.tsx
@@ -0,0 +1,9 @@
+export default function PageHeadGoogleFonts () {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='crossOrigin' />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alegreya+SC:wght@800&family=Alegreya+Sans:ital,wght@0,300;0,700;1,300;1,700&family=Alegreya:ital,wght@0,400;0,700;1,400;1,700&display=swap" />
+    </>
+  )
+}
