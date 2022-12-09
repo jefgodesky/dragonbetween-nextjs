58b3a1db8114681ab057708155c0c7967107eb72
Jason Godesky <jefgodesky@gmail.com>
2022-12-08 21:18:23 -0500

    Add new component: PageHeadOG

---
+++ components/PageHeadOG/PageHeadOG.tsx
@@ -0,0 +1,26 @@
+interface PageHeadOGProps {
+  title?: string
+  path?: string
+  description?: string
+  image?: string
+}
+
+export default function PageHeadOG (props: PageHeadOGProps) {
+  const title = props.title ?? 'The Dragon Between'
+  const url = props.path !== undefined ? `http://dragonbetween.net${props.path}` : 'https://dragonbetween.net'
+  const description = props.description ?? 'Welcome to Eberron.'
+  const image = props.image ?? 'https://dragonbetween.net/img/social.jpg'
+
+  return (
+    <>
+      <meta property='og:title' content={title} />
+      <meta property='og:site_name' content='The Dragon Between' />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content={url} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={image} />
+    </>
+  )
+}
+
+export type { PageHeadOGProps }
