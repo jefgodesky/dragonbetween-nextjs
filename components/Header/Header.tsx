f07eefaae42882ac47b78faefd1ca1ffc534c1a1
Jason Godesky <jefgodesky@gmail.com>
2022-12-08 21:38:54 -0500

    Style page title and header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header () {
   return (
     <header>
-      <h1><a href='/'>The Dragon Between</a></h1>
+      <h1>
+        <a href='/'>
+          <img src='/img/dragonbetween.png' alt='The Dragon Between' />
+        </a>
+      </h1>
     </header>
   )
 }
