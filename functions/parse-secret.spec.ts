266df1cfdfc1a32caf94f35edf199b37e29973df
Jason Godesky <jefgodesky@gmail.com>
2022-12-10 14:38:53 -0500

    parseSecret: Handle nested secrets

--- functions/parse-secret.spec.ts
+++ functions/parse-secret.spec.ts
@@ -1,16 +1,27 @@
 import { isSecret } from '../types/Secret'
 import parseSecret from './parse-secret'
 
+const testSecret = (condition: string, text: string, orig: string): void => {
+  const actual = parseSecret(orig)
+  expect(isSecret(actual)).toBe(true)
+  expect(actual?.condition).toEqual(condition)
+  expect(actual?.orig).toEqual(orig)
+  expect(actual?.text).toEqual(text)
+}
+
 describe('parseSecret', () => {
   it('parses a secret', () => {
     const condition = 'secret1'
     const text = 'shhhh'
     const orig = `{{if ${condition}}}${text}{{/if}}`
-    const actual = parseSecret(orig)
-    expect(isSecret(actual)).toBe(true)
-    expect(actual?.condition).toEqual(condition)
-    expect(actual?.orig).toEqual(orig)
-    expect(actual?.text).toEqual(text)
+    testSecret(condition, text, orig)
+  })
+
+  it('parses nested secrets', () => {
+    const condition = 'secret1'
+    const text = 'shhhh {{if secret2}}shhhhhh{{/if}}'
+    const orig = `{{if ${condition}}}${text}{{/if}}`
+    testSecret(condition, text, orig)
   })
 
   it('returns null if there is no secret', () => {
--- functions/parse-secret.ts
+++ functions/parse-secret.ts
@@ -1,7 +1,7 @@
 import Secret from '../types/Secret'
 
 export default function parseSecret (orig: string): Secret | null {
-  const elements = orig.match(/{{if ((.|\n)*?)}}((.|\n)*?){{\/if}}/m)
+  const elements = orig.match(/^{{if ((.|\n)*?)}}((.|\n)*?){{\/if}}$/m)
   if (elements === null || elements.length < 5) return null
   return { condition: elements[1], orig, text: elements[3] }
 }
