6eef38f76232d653dd853a6a074c1fd558995ce0
Jason Godesky <jefgodesky@gmail.com>
2022-12-15 21:29:30 -0500

    Fix linting errors

--- components/PageHeadFavicons/PageHeadFavicons.tsx
+++ components/PageHeadFavicons/PageHeadFavicons.tsx
@@ -3,12 +3,12 @@ import { ReactElement } from 'react'
 export default function PageHeadFavicons (): ReactElement {
   return (
     <>
-      <link rel="icon" href="/favicon-black/favicon.ico" />
-      <link rel="icon" media="(prefers-color-scheme: dark)" href="/favicon-white/favicon.ico" />
-      <link rel="apple-touch-icon" href="/favicon-black/logo192.png" />
-      <link rel="apple-touch-icon" media="(prefers-color-scheme: dark)" href="/favicon-white/logo192.png" />
-      <link rel="manifest" href="/manifest-light.json" />
-      <link rel="manifest" media="(prefers-color-scheme: dark)" href="/manifest-dark.json" />
+      <link rel='icon' href='/favicon-black/favicon.ico' />
+      <link rel='icon' media='(prefers-color-scheme: dark)' href='/favicon-white/favicon.ico' />
+      <link rel='apple-touch-icon' href='/favicon-black/logo192.png' />
+      <link rel='apple-touch-icon' media='(prefers-color-scheme: dark)' href='/favicon-white/logo192.png' />
+      <link rel='manifest' href='/manifest-light.json' />
+      <link rel='manifest' media='(prefers-color-scheme: dark)' href='/manifest-dark.json' />
     </>
   )
 }
